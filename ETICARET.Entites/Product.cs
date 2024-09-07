da23be60f47f0455af0cef73275a509957b22908
tahsincanpolat <tahsincanpolat@gmail.com>
2024-09-07 14:04:15 +0300

    entities

---
+++ ETICARET.Entites/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ETICARET.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Image> Images { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+        public List<Comment> Comments { get; set; }
+
+        public Product()
+        {
+            Images = new List<Image>();
+            ProductCategories = new List<ProductCategory>();
+            Comments = new List<Comment>();
+        }
+    }
+}
