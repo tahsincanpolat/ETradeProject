4c51540a9cd1645550b948c12d999f2cb81f7216
tahsincanpolat <tahsincanpolat@gmail.com>
2024-10-05 15:50:57 +0300

    comments edit,delete,create, login page

---
+++ ETICARET.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using ETICARET.Business.Abstract;
+using ETICARET.WebUI.Identity;
+using ETICARET.WebUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETICARET.WebUI.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private ICartService _cartService;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ICartService cartService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _cartService = cartService;
+        }
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(
+                new LoginModel()
+                {
+                    ReturnUrl = returnUrl,
+                }    
+            );
+        }
+    }
+}
